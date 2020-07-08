40a95526a7c61be30b50872d54042d9f8a9288d1
Jinendra Sontakke <63655965+sjinu17@users.noreply.github.com>
2020-07-08 19:55:41 +0530

    initial commit

--- short.go
+++ short.go
@@ -1,99 +1,65 @@
-package main
+package arraySort
 
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strings"
-  "strconv"
-  "sort"
-  "sync"
+	"fmt"
 )
 
-func sub_sort(wg *sync.WaitGroup, is []int) {
-  fmt.Println(is)
-  sort.Ints(is)
-  wg.Done()
+func main() {
+	arr := make([]int, 12)
+	for i := 0; i < 12; i++ {
+		fmt.Println("Enter a Int.")
+		fmt.Scan(&arr[i])
+	}
+	part1 := len(arr) / 4
+	middle := len(arr) / 2
+	part3 := part1 + middle
+	c := make(chan []int, 4)
+	go sort(arr[:part1], c)
+	go sort(arr[part1:middle], c)
+	go sort(arr[middle:part3], c)
+	go sort(arr[part3:], c)
+	arr1 := <-c
+	arr2 := <-c
+	arr3 := <-c
+	arr4 := <-c
+	final := merge(merge(arr1, arr2), merge(arr3, arr4))
+	fmt.Println(final)
 }
 
-func main() {
-  fmt.Println("Please input some integers to sort: ")
-  br := bufio.NewReader(os.Stdin)
-  a, _, _ := br.ReadLine()
-  ns := strings.Split(string(a), " ")
-  var numbers []int
-  for _, s := range(ns) {
-    n, _ := strconv.Atoi(s)
-    numbers = append(numbers, n)
-  }
-  size := len(numbers) / 4
-  var wg sync.WaitGroup
-  for c := 0; c < 4; c++ {
-    wg.Add(1)
-    if c != 3 {
-      go sub_sort(&wg, numbers[c*size: (c+1)*size])
-    } else {
-      go sub_sort(&wg, numbers[c*size:])
-    }
-  }
-  wg.Wait()
+func sort(arr []int, c chan []int) {
+	for j := 0; j < len(arr)-1; j++ {
+		for i := j; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+			}
+		}
+	}
+	c <- arr
+}
 
-  var sorted []int
-  c1 := numbers[: 1*size]
-  c2 := numbers[1*size : 2*size]
-  c3 := numbers[2*size : 3*size]
-  c4 := numbers[3*size :]
-  for k := 0; k < len(numbers); k++ {
-    i := 0
-    j := 0
-    if len(c1) !=  0 {
-      i = c1[0]
-      j = 1
-    }
-    if len(c2) != 0 {
-      if j == 0 {
-        i = c2[0]
-        j = 2
-      } else {
-        if c2[0] < i {
-          i = c2[0]
-          j = 2
-          }
-      }
-    }
-    if len(c3) != 0 {
-      if j == 0 {
-        i = c3[0]
-        j = 3
-      } else {
-        if c3[0] < i {
-          i = c3[0]
-          j = 3
-          }
-      }
-    }
-    if len(c4) != 0 {
-      if j == 0 {
-        i = c4[0]
-        j = 4
-      } else {
-        if c4[0] < i {
-          i = c4[0]
-          j = 4
-          }
-      }
-    }
-    sorted = append(sorted, i)
-    switch j {
-    case 1:
-      c1 = c1[1 :]
-    case 2:
-      c2 = c2[1 :]
-    case 3:
-      c3 = c3[1 :]
-    case 4:
-      c4 = c4[1 :]
-    }
-  }
-  fmt.Println(sorted)
+func merge(left, right []int) []int {
+	result := make([]int, len(left)+len(right))
+
+	i := 0
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] < right[0] {
+			result[i] = left[0]
+			left = left[1:]
+		} else {
+			result[i] = right[0]
+			right = right[1:]
+		}
+		i++
+	}
+
+	for j := 0; j < len(left); j++ {
+		result[i] = left[j]
+		i++
+	}
+	for j := 0; j < len(right); j++ {
+		result[i] = right[j]
+		i++
+	}
+
+	return result
 }
